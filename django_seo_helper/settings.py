a8c5195b29a8136ce6ec7aba3a806c3fa5039254
pragiark <pragiark@wp.pl>
2019-12-14 16:51:48 +0100

    add app to installed_aps in settings.py

--- django_seo_helper/settings.py
+++ django_seo_helper/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#my apps
+    'dsh_scrapper.apps.DshScrapperConfig',
 ]
 
 MIDDLEWARE = [
