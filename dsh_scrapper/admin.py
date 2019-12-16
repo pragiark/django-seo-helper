9184ba283a305b3a46e05b392445e66ecb81afa5
pragiark <pragiark@wp.pl>
2019-12-16 22:48:25 +0100

    adding decorators name,url,created

--- dsh_scrapper/admin.py
+++ dsh_scrapper/admin.py
@@ -3,4 +3,6 @@ from dsh_scrapper.models import Client
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url')
+    list_display = ('name', 'url', 'created')
+    list_filter = ('name', 'url')
+    search_fields = ('name', 'url')
