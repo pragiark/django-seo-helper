e8fb3e6de8d06b3cbb5faebf28d00ef010a0d9f2
pragiark <pragiark@wp.pl>
2019-12-16 20:45:57 +0100

    add decorator and cusomize admin panel

--- dsh_scrapper/admin.py
+++ dsh_scrapper/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from dsh_scrapper.models import Client
 
-admin.site.register(Client)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
