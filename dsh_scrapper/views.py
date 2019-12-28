5acddba0b0614d6cbc130492d3e78c8e25e95df9
Maciej Kostuch <kostuchmaciek@gmail.com>
2019-12-28 19:29:43 +0100

    base for add client view

--- dsh_scrapper/views.py
+++ dsh_scrapper/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class AddClientView(View):
+    template = 'django_seo_helper/add_client.html'
+
+    def get(self, request):
+        # TODO: create file forms.py, define there form based on Model, then send form to template
+        # TODO: in template - display for content by `form.as_p` e.g.
+        return render(request, self.template)
+
+    def post(self, request):
+        # TODO: when post send
+        # TODO: redirect to same url when success and add message using django.contrib.messages.
+        pass
