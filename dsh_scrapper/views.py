8b1390faaf70a6e88f4db81fce5cbdbc1a09efe8
pragiark <pragiark@wp.pl>
2020-01-04 14:13:02 +0100

    add form to view

--- dsh_scrapper/views.py
+++ dsh_scrapper/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-
+from .forms import AddClientForm
 
 class AddClientView(View):
     template = 'django_seo_helper/add_client.html'
 
     def get(self, request):
         # TODO: create file forms.py, define there form based on Model, then send form to template
+        form = AddClientForm()
         # TODO: in template - display for content by `form.as_p` e.g.
         return render(request, self.template)
 
