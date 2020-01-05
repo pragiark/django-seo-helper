1320bed0cb1ae2fa3b3265adc26ce8c77d086bdb
Maciej Kostuch <kostuchmaciek@gmail.com>
2020-01-05 16:26:52 +0100

    small modifications

--- dsh_scrapper/views.py
+++ dsh_scrapper/views.py
@@ -1,28 +1,23 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
+
 from .forms import AddClientForm
-from django.http import HttpResponseRedirect
-from django.contrib import messages
 
 
 class AddClientView(View):
     template = 'django_seo_helper/add_client.html'
 
-
     def get(self, request):
-        # TODO: in template - display for content by `form.as_p` e.g.
         form = AddClientForm()
         return render(request, self.template, {'form': form})
 
-
-
     def post(self, request):
-        # TODO: when post send
         form = AddClientForm(request.POST)
         if form.is_valid():
             new_form = form.save(commit=False)
             new_form.save()
-            # TODO: redirect to same url when success and add message using django.contrib.messages.
             messages.success(request, 'Udało się dodać')
+            # TODO: redirect to same url when success and add message using django.contrib.messages.
             return render(request, self.template, {'form': form})
-        pass
+        return render(request, self.template, {'form': form})
