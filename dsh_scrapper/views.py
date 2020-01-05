b2863b54a6f3c1bc27923f3bf22f6a2d9262f20b
pragiark <pragiark@wp.pl>
2020-01-05 15:58:00 +0100

    move post sens section to def post, add success massage

--- dsh_scrapper/views.py
+++ dsh_scrapper/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import AddClientForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 
 class AddClientView(View):
     template = 'django_seo_helper/add_client.html'
 
-    def get(self, request):
-        if request.method == "GET":
-            form = AddClientForm(request.GET)
-            if form.is_valid():
-                new_form = form.save(commit=False)
-                new_form.save()
-            # TODO create new template and return redirect to "detail" or sth
 
-        # TODO: create file forms.py, define there form based on Model, then send form to template
-        else:
-            form = AddClientForm()
+    def get(self, request):
+        # TODO: in template - display for content by `form.as_p` e.g.
+        form = AddClientForm()
         return render(request, self.template, {'form': form})
 
-            # TODO: in template - display for content by `form.as_p` e.g.
+
 
     def post(self, request):
         # TODO: when post send
-        # TODO: redirect to same url when success and add message using django.contrib.messages.
+        form = AddClientForm(request.POST)
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.save()
+            # TODO: redirect to same url when success and add message using django.contrib.messages.
+            messages.success(request, 'Udało się dodać')
+            return render(request, self.template, {'form': form})
         pass
