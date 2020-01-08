37f7c90f38feb0306bd6a48d11b86a48c894d19d
pragiark <pragiark@wp.pl>
2020-01-08 21:55:47 +0100

    add Pagestatus to admin.py

--- dsh_scrapper/admin.py
+++ dsh_scrapper/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from dsh_scrapper.models import Client
+from .models import Client, PageStatus
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'created')
     list_filter = ('name', 'url')
     search_fields = ('name', 'url')
+
+@admin.register(PageStatus)
+class PageStatusAdmin(admin.ModelAdmin):
+    list_display = ('body', 'status', 'client', 'created')
