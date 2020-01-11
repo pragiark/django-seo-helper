7e523f23f96d1069ca7b01620cba4f13481f506c
pragiark <pragiark@wp.pl>
2020-01-11 09:11:32 +0100

    creating the first action in manage.py (add record via manage.py)

---
+++ dsh_scrapper/management/commands/add_record.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from dsh_scrapper.models import Client
+
+class Command(BaseCommand):
+    help = 'Add record do database using django-admin commands'
+
+    def add_arguments(self, parser):
+       parser.add_argument('name')
+       parser.add_argument('url')
+
+    def handle(self, *args, **options):
+        name_var = options['name']
+        url_var = options['url']
+        sucess_var = Client.objects.create(name=name_var, url=url_var)
+        self.stdout.write(self.style.SUCCESS('Successfully add %s' % sucess_var))
